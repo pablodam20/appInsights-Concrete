bf02515ff240ccbb2c1356e03c207adfbdf7434f
chirislash <chirislash@outlook.com>
2015-03-25 01:44:56 -0600

    Revert "Add the directory appinsights"
    This reverts commit 59a088a22bf1f9a3d904fe1921392ee89d48f2fb.

---
+++ controller.php
@@ -0,0 +1,26 @@
+<?php 
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class AppInsightsAddOnPackage extends Package {
+
+     protected $pkgHandle = 'app_insights_addon';
+     protected $appVersionRequired = '5.6.0';
+     protected $pkgVersion = '1.0';
+
+     public function getPackageDescription() {
+          return t("Integrates a Concrete5 site with Microsoft Application Insights");
+     }
+
+     public function getPackageName() {
+          return t("Application Insights Add-On");
+     }
+    
+     public function install() {
+          $pkg = parent::install();
+    
+          BlockType::installBlockTypeFromPackage('app_insights_addon', $pkg);
+     }
+}
+
+?>
