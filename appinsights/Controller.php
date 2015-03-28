e0d25cd6e4be874ca6aa6fcc5d7ce5f7807aa37c
chirislash <chirislash@outlook.com>
2015-03-28 16:17:51 -0600

    Added the events
    The event for the server side
    The event for the client side

--- appinsights/Controller.php
+++ appinsights/Controller.php
@@ -1,6 +1,9 @@
 <?php
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
+require_once 'vendor/autoload.php';
+require_once 'vendor/microsoft/application-insights/vendor/autoload.php';
+
 class AppInsightsPackage extends Package{
     
     protected $pkgHandle = 'appinsights';
@@ -16,25 +19,38 @@ class AppInsightsPackage extends Package{
 	}
     
 	public function on_start() {
-	$_instrumentationKey = 'Poner aca Instrumentation Key';
-	$serverInstrumentation = new \packages\appinsights\src\Server_Instrumentation($_instrumentationKey);
-	Events::extend('on_render_complete', 'Server_Instrumentation', 'endRequest', 'packages/' . $this->pkgHandle . '/src/Server_Instrumentation.php',array("Poner_aca_la_IK"));
+		Events::extend('on_render_complete', 'AppInsightsPackage', 'on_render_complete', 'packages/'.$this->pkgHandle.'/Controller.php');
+		Events::extend('on_before_render', 'AppInsightsPackage', 'on_before_render', 'packages/'.$this->pkgHandle.'/Controller.php');
+
+	}
+	public function on_before_render() {
+		$page = Page::getCurrentPage();
+		if (!$page->isEditMode())
+		{
+        // Enables client-side instrumentation
+		$_instrumentationKey = 'Poner_Aca_IK';
+        $clientInstrumentation = new ApplicationInsights\Joomla\Client_Instrumentation();
+        $clientInstrumentation->addPrefix($_instrumentationKey,$page->getCollectionName());
+		}
+	}
+	public function on_render_complete(){
+		$page = Page::getCurrentPage();
+		if (!$page->isEditMode())
+		{
+		// Enables server-side instrumentation
+		$_instrumentationKey = 'Poner_Aca_IK';
+		$serverInstrumentation = new ApplicationInsights\Joomla\Server_Instrumentation($_instrumentationKey,$page->getCollectionName());
+		register_shutdown_function(array($serverInstrumentation, 'endRequest'));
+		}
 	}
 
 	
 	public function install() {
 		$pkg = parent::install();
-		
 		// install block		
 		BlockType::installBlockTypeFromPackage('appinsights', $pkg);
 	}
 	
-	//Determine if its on edit mode or not.
-	$c; if ($c->isEditMode()) {
-	   //edit mode is currently being displayed.
-	} 
-	else { 
-		//Edit mode is not being shown.
-	}
+
     
 }
